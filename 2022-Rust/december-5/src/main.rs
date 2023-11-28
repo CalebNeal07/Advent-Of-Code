f2ba7a2531d4af65fe751369f7864783ed36a880
CalebNeal07 <caleboneal07@gmail.com>
2023-11-28 17:46:50 -0500

    2022-december5 WIP

--- 2022-Rust/december-5/src/main.rs
+++ 2022-Rust/december-5/src/main.rs
@@ -1,13 +1,21 @@
-use std::io::stdin;
+use std::{collections::VecDeque, io::stdin};
 
 fn main() {
     let input = stdin().lines();
-    let
+    let mut cargo = "".to_string();
+    let mut ships: Vec<VecDeque<char>> = vec![];
 
     for l in input {
-        let line = l.unwrap().chars();
+        let line = l.unwrap().to_string();
 
+        if line == "".to_string() {
+            break;
+        }
 
+        cargo += &(cargo.clone() + "\r\n" + &line);
 
+        for line in cargo.lines().skip(1) {
+            for i in (0..line.len()).step_by(4) {}
+        }
     }
 }
